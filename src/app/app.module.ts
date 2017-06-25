2a392e850d7704efb280242f00b6de75d4cfe529
MartinGonz <martin.m.gonz@gmail.com>
2017-06-25 04:39:21 -0300

    loging google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import {AF} from "./providers/af";
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD6Qg7tUhcpogn2rjVklNf2wMM3OS3jxRM',
+  authDomain: 'merp-64b26.firebaseapp.com',
+  databaseURL: 'https://merp-64b26.firebaseio.com',
+  storageBucket: 'merp-64b26.appspot.com',
+  messagingSenderId: '195433682352'
+};
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     AngularFireModule.initializeApp(firebaseConfig),
+     RouterModule.forRoot(routes),
+     AngularFireDatabaseModule,
+     AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AF],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
